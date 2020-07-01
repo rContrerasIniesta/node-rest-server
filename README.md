4035abdd7739a9c79a453272e81e30c5ffd97274
Rafa <contreras.iniesta@gmail.com>
2020-07-01 19:33:28 +0200

    primera subida

---
+++ README.md
@@ -0,0 +1,1 @@
+Aplicacion NODE rest server
---
+++ server/server.js
@@ -0,0 +1,40 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/usuario', function(req, res) {
+    res.json('get usuarios');
+});
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'Nombre no informado'
+        });
+    } else {
+        res.json(body);
+    }
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete usuarios');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto', process.env.PORT);
+});
